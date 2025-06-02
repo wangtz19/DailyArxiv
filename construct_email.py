e2a5835dbdd529d17524e8af73c0b8c0bb6df7e4
wangtz <wangtz19@outlook.com>
2025-06-02 22:20:58 +0800

    first commit

---
+++ construct_email.py
@@ -0,0 +1,123 @@
+from arxiv import Result
+from tqdm import tqdm
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
+import datetime
+from loguru import logger
+
+
+framework = """
+<!DOCTYPE HTML>
+<html>
+<body>
+
+<div>
+    __CONTENT__
+</div>
+
+<br><br>
+<div>
+To unsubscribe, remove your email in your Github Action setting.
+</div>
+
+</body>
+</html>
+"""
+
+
+def get_empty_html():
+  block_template = """
+  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family: Arial, sans-serif; border: 1px solid #ddd; border-radius: 8px; padding: 16px; background-color: #f9f9f9;">
+  <tr>
+    <td style="font-size: 20px; font-weight: bold; color: #333;">
+        No Papers Today. Take a Rest!
+    </td>
+  </tr>
+  </table>
+  """
+  return block_template
+
+
+def get_block_html(title: str, authors: str, updated: str, arxiv_id: str, abstract: str, pdf_url: str):
+    block_template = """
+    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family: Arial, sans-serif; border: 1px solid #ddd; border-radius: 8px; padding: 16px; background-color: #f9f9f9;">
+    <tr>
+        <td style="font-size: 20px; font-weight: bold; color: #333;">
+            {title}
+        </td>
+    </tr>
+    <tr>
+        <td style="font-size: 14px; color: #666; padding: 8px 0;">
+            {authors}
+            <br>
+            <i>{updated}</i>
+        </td>
+    </tr>
+    <tr>
+        <td style="font-size: 14px; color: #333; padding: 8px 0;">
+            <strong>arXiv ID:</strong> {arxiv_id}
+        </td>
+    </tr>
+    <tr>
+        <td style="font-size: 14px; color: #333; padding: 8px 0;">
+            <strong>abstract:</strong> {abstract}
+        </td>
+    </tr>
+
+    <tr>
+        <td style="padding: 8px 0;">
+            <a href="{pdf_url}" style="display: inline-block; text-decoration: none; font-size: 14px; font-weight: bold; color: #fff; background-color: #d9534f; padding: 8px 16px; border-radius: 4px;">PDF</a>
+        </td>
+    </tr>
+</table>
+"""
+    return block_template.format(title=title, authors=authors, updated=updated,
+                                 arxiv_id=arxiv_id, abstract=abstract, pdf_url=pdf_url)
+
+
+def render_email(papers:list[Result]):
+    parts = []
+    if len(papers) == 0 :
+        return framework.replace('__CONTENT__', get_empty_html())
+    
+    for p in tqdm(papers,desc='Rendering Email'):
+        authors = [a.name for a in p.authors[:5]]
+        authors = ', '.join(authors)
+        if len(p.authors) > 5:
+            authors += ', ...'
+        parts.append(get_block_html(p.title, authors, p.updated.strftime('%Y-%m-%d %H:%M:%S'),
+                                    p.entry_id.split('/')[-1], p.summary, p.pdf_url))
+
+    content = '<br>' + '</br><br>'.join(parts) + '</br>'
+    return framework.replace('__CONTENT__', content)
+
+
+def send_email(sender:str, receiver:str, password:str, 
+               smtp_server:str, smtp_port:int, html:str,):
+    def _format_addr(s):
+        name, addr = parseaddr(s)
+        return formataddr((Header(name, 'utf-8').encode(), addr))
+
+    msg = MIMEText(html, 'html', 'utf-8')
+    msg['From'] = _format_addr('Github Action <%s>' % sender)
+    msg['To'] = _format_addr('You <%s>' % receiver)
+    today = datetime.datetime.now().strftime('%Y/%m/%d')
+    msg['Subject'] = Header(f'Daily arXiv {today}', 'utf-8').encode()
+
+    logger.info(f"Connecting to SMTP server {smtp_server}:{smtp_port}...")
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port, timeout=20)
+        server.starttls()
+    except Exception as e:
+        logger.warning(f"Failed to use TLS. {e}")
+        logger.warning(f"Try to use SSL.")
+        server = smtplib.SMTP_SSL(smtp_server, smtp_port)
+    
+    logger.info("Connected to SMTP server.")
+    resp = server.login(sender, password)
+    logger.info(f"Login resp: {resp}.")
+    resp = server.sendmail(sender, [receiver], msg.as_string())
+    logger.info(f"Sending resp: {resp}.")
+    server.quit()
---
+++ main.py
@@ -0,0 +1,70 @@
+import arxiv
+import argparse
+import os
+import sys
+from dotenv import load_dotenv
+load_dotenv(override=True)
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+from construct_email import render_email, send_email
+from loguru import logger
+
+
+def get_arxiv_paper(keyword: str, max_results: 10, ):
+    client = arxiv.Client(num_retries=10, delay_seconds=10)
+    search = arxiv.Search(
+        query=f'(ti:"{keyword}" OR abs:"{keyword}") AND (cat:cs.AI OR cat:cs.LG OR cat:cs.DC OR cat:cs.NI OR cat:cs.PF)',
+        max_results=max_results,
+        sort_by=arxiv.SortCriterion.LastUpdatedDate
+    )
+    papers = []
+    for result in client.results(search):
+        papers.append(result)
+    return papers
+
+
+parser = argparse.ArgumentParser(description="Daily Arxiv Paper for Networking")
+
+def add_argument(*args, **kwargs):
+    def get_env(key: str, default=None):
+        # handle environment variables generated at Workflow runtime
+        # Unset environment variables are passed as '', we should treat them as None
+        v = os.environ.get(key)
+        if v == '' or v is None:
+            return default
+        return v
+    parser.add_argument(*args, **kwargs)
+    arg_full_name = kwargs.get('dest',args[-1][2:])
+    env_name = arg_full_name.upper()
+    env_value = get_env(env_name)
+    if env_value is not None:
+        #convert env_value to the specified type
+        if kwargs.get('type') == bool:
+            env_value = env_value.lower() in ['true','1']
+        else:
+            env_value = kwargs.get('type')(env_value)
+        parser.set_defaults(**{arg_full_name:env_value})
+
+
+if __name__ == '__main__':
+    add_argument('--send_empty', type=bool, help='If get no arxiv paper, send empty email', default=False)
+    add_argument('--max_results', type=int, help='Maximum number of papers to recommend', default=10)
+    add_argument('--arxiv_keyword', type=str, help='Arxiv search query')
+    add_argument('--smtp_server', type=str, help='SMTP server')
+    add_argument('--smtp_port', type=int, help='SMTP port')
+    add_argument('--sender', type=str, help='Sender email address')
+    add_argument('--receiver', type=str, help='Receiver email address')
+    add_argument('--sender_password', type=str, help='Sender email password')
+    args = parser.parse_args()
+    
+    logger.remove()
+    logger.add(sys.stdout, level="INFO")
+    logger.info("Retrieving Arxiv papers...")
+    papers = get_arxiv_paper(args.arxiv_keyword, args.max_results)
+    if len(papers) == 0:
+        logger.info("No new papers found. Yesterday maybe a holiday and no one submit their work :). If this is not the case, please check the ARXIV_QUERY.")
+        if not args.send_empty:
+          exit(0)
+    html = render_email(papers)
+    logger.info("Sending email...")
+    send_email(args.sender, args.receiver, args.sender_password, args.smtp_server, args.smtp_port, html)
+    logger.success("Email sent successfully! If you don't receive the email, please check the configuration and the junk box.")
